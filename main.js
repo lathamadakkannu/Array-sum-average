6426f7510a0400c8de1b9c7c9bbacd869082171b
lathamadakkannu <lathamadakkannu@gmail.com>
2023-07-19 19:33:01 -0700

    addnew

---
+++ main.js
@@ -0,0 +1,13 @@
+var arr=[11,10,70,75,59,2,9];
+sum=0;
+document.write("arr=[11,10,70,75,59,2,9]" + "<br>" + "<br>");
+
+	for(let i=0; i<arr.length; i++)
+	{
+		sum=sum+arr[i];
+	}
+			document.write(sum + "<br>" + "<br>");
+			document.write(average=sum/arr.length);
+		
+		
+	
